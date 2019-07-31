9a58a703fb5391a48e0783936387b70b92d1d848
Kat9274 <33795714+kat9274@users.noreply.github.com>
2019-07-31 16:50:04 -0500

    Client Test

---
+++ Client.py
@@ -0,0 +1,30 @@
+import socket, json
+
+while True:
+    try:
+        Server = "192.168.1.100" #input("What IP do you want to connect to? >>> ")
+        Port = 5000
+        
+        ClientSocket = socket.socket()
+        ClientSocket.connect((Server, Port))
+        break
+    except ConnectionRefusedError:
+        print("Server not running... try again")
+        pass
+    except socket.gaierror:
+        print("Not a valid ip... try again")
+        pass
+
+Recive = ["", "", ""]
+
+while Recive[0] != "GameOver":
+    Recive = ClientSocket.recv(1024).decode()
+    Recive = Recive.split(",")
+    if Recive[0] == socket.gethostbyname(socket.gethostname()):
+        if Recive[2] == "Input":
+            ClientSocket.send(input(Recive[1]).encode())
+        elif Recive[2] == "Print":
+            print(Recive[1])
+        
+print(Recive[1])    
+ClientSocket.close()  #Close the connection after the server sends "GameOver"
