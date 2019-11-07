a2cde19bfbc1d577459eacba4ad35068f27a025f
Kat9274 <33795714+kat9274@users.noreply.github.com>
2019-11-07 11:03:54 -0600

    Version 0.0.3

--- uno.py
+++ uno.py
@@ -1,23 +1,20 @@
 from client import *
 
-GameOver = False
-
 def Parse():
     global GameOver
-    Message = Get().split('`')
-    print(Message[0])
-    if Message[1].lower() == "y":
-        Send(input(Message[2]))
-    elif Message[1].lower() == "GameOver":
+    Message = Get().decode().split('`')
+    print(f"{Message[0]}")
+    if Message[1] == 'y':
+        Send(f"{input(f'{Message[2]}')}")
+    elif Message[1] == 'g':
         GameOver = True
-    return Message
 
 while True:
-    Host = str(input("Server Ip >>> "))
-    Status = Connect(Host, 9274)
+    Status = Connect(input(f"Server >>> "), 9274)
     if Status == 0:
-        print("Connected.")
+        print(f"Connected.")
         break
 
+GameOver = False
 while not GameOver:
     Parse()
